d0c436a40b9776c5a1072d69c9658d7c1386baf2
elena_shlegel <elenashlegel@gmail.com>
2018-07-27 03:05:11 +0300

    ClassWorkSolution

---
+++ Program/CarRepository/Car2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepository
+{
+    public partial class Car
+    {
+
+
+        public string Prise
+        {
+            get
+            {
+                return _prise.ToString();
+            }
+            set
+            {
+                double prise;
+                if ((Double.TryParse(value, out prise)))
+                {
+                    _prise = prise;
+                }
+                else
+                {
+                    throw new System.ArgumentException("Value is not double");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new System.ArgumentException("Value can not be null");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new System.ArgumentException("Value can not be null");
+                }
+                else
+                {
+                    _color = value;
+                }
+
+            }
+        }
+
+
+
+
+        public void Discount(int discount = 10)
+        {
+            this.Disc(discount);
+        }
+
+    }
+}
