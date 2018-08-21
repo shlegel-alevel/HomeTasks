a585543a46935df4d7016fb37802390be9ac9179
elena_shlegel <elenashlegel@gmail.com>
2018-08-22 00:01:56 +0300

    Whole func

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -10,6 +10,44 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string[] array = { "(", "(", ")", ")" };
+
+                Console.WriteLine(IfMassiveIsValid(array));
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        private static bool IfMassiveIsValid (string [] array)
+        {
+            var result = true;
+            var counter=0;
+            for (int i=0; i<array.Length; i++)
+            {
+                if (array[i] == "(")
+                    counter++;
+                else if (array[i] == ")")
+                     counter--;
+                else 
+                throw new ArgumentException ("Incorect symbols");
+                if (counter <0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            if (counter != 0)
+                result = false;
+            return result;
         }
     }
 }
