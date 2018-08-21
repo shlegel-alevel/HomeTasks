a934d3d59980545321fa612b91256a097080404e
Shlegel <elenashlegel@gmail.com>
2018-08-21 06:33:48 +0300

    first

---
+++ Program/Program/ITimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public interface ITimer
+    {
+        /// <summary>
+        /// Pass minutes when alarm will be rised
+        /// example: SetAlarm(30) will rize alarm in 30 minutes
+        /// </summary>
+        /// <param name="minutes"></param>
+        void SetAlarm(int minutes);
+
+        /// <summary>
+        /// Print out time in nice format
+        /// </summary>
+        void ShowCurrentTime();
+    }
+}
---
+++ Program/Program/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Timer : ITimer
+    {
+        private int _alarmTime;
+        private int _currentTime;
+
+        public Timer ()
+
+        void SetAlarm(int minutes);
+        void ShowCurrentTime();
+
+
+        private void Counter()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                do
+                {
+                    //Sleep for one second
+                    Thread.Sleep(1000);
+                    //And increment our inner counter
+                    _currentTime += 1;
+
+                    if (_alarmTime == _currentTime) OnAlarm?.Invoke();
+                } while (true);
+            });
+        }
+    }
+}
